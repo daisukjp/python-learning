d3dae522488ba1b971776626bd31e1087c4efa60
Daisuke Sato <dsato2@nait.ca>
2022-09-12 06:01:45 -0600

    strings done

---
+++ string-basic.py
@@ -0,0 +1,17 @@
+# Strings
+
+# both single and double are okay for print
+print("Hello world!")
+print(1)
+
+print("I'm fine")
+print('I"m fine')
+
+print("""Hello World
+How are you doing?""")
+
+print("Hello \nworld")
+
+print("Hello \tworld")
+
+print("Hello" + "World" + "!!")
