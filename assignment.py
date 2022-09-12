02f15b4e63bb1dfd60e6eee4cea5172c4ad1bdad
Daisuke Sato <dsato2@nait.ca>
2022-09-12 06:32:57 -0600

    completed some courses

---
+++ assignment.py
@@ -0,0 +1,25 @@
+# variables: assignment
+# print("Hello World")
+
+hello = "Hello"
+world = "World"
+
+print(hello + world)
+
+# format
+print("{} {}".format(hello, world))
+
+name = "Emily"
+
+print("Hey, {}!! How are you doing?".format(name))
+
+balance = 100
+
+print("balance: {}".format(balance))
+
+# fstring
+print(f"{hello} {world}")
+
+print(f"Hey, {name}!! How are you doing?")
+
+print(f"balance: {balance}")
