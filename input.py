8f8112d40f69e359178fb582b4cc4a71850d69e1
Daisuke Sato <dsato2@nait.ca>
2022-10-03 07:53:46 -0600

    added input

---
+++ input.py
@@ -0,0 +1,14 @@
+# input()
+
+# age = input("How old are you?")
+# print("your are {} years old".format(age))
+
+
+# challenge1
+age = int(input("How old are yopu?"))
+# age = int(age)
+
+if age >= 18:
+    print("You cna enter the casino")
+else: 
+    print("You can not enter this casino")
