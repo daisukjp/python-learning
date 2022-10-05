e421b1d332a419f0db6589b2bae23bd8953d0fd7
Daisuke Sato <dsato2@nait.ca>
2022-10-05 06:03:38 -0600

    added slicing

---
+++ slicing.py
@@ -0,0 +1,18 @@
+even = [2, 4, 6, 8, 10, 12]
+print(even[1:4])
+
+print(even[0:4])
+print(even[:4])
+
+print(even[3:-1])
+
+print(even[3:])
+
+print(even[:])
+
+text = "getting better"
+print(text[3:])
+
+print(text[2:20:2])
+
+print(text[::-1])
