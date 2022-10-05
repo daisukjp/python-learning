baa4fe346bae7b81295b272162a902cab33a8a22
Daisuke Sato <dsato2@nait.ca>
2022-10-05 05:51:34 -0600

    list method added

--- list.py
+++ list.py
@@ -2,7 +2,35 @@ fruits = ['apple', 'peach', 'melon', 'grapes']
 
 hetro_list = ['string', 1, 3.4, True, fruits]
 
-print(hetro_list)
-print(fruits[0])
-print(fruits[-1])
-print(hetro_list[-1][-1])
+
+# print(hetro_list)
+# print(fruits[0])
+# print(fruits[-1])
+# print(hetro_list[-1][-1])
+
+# 26. list method
+
+# append
+print(fruits)
+fruits.append('banana')
+print(fruits)
+
+# insert
+print(fruits)
+fruits.insert(3, 'lemon')
+print(fruits)
+
+# delete
+fruits.remove('peach')
+print(fruits)
+
+# sort 1
+fruits.sort()
+print(fruits)
+
+# sort2
+fruits.sort(reverse=True)
+print(fruits)
+
+# len 
+print(len(fruits))
